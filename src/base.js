4e8d2d71a9840c76b3c7b8a4306b5f63af6e9399
Michael <mm8775@nyu.edu>
2023-02-19 15:14:17 -0600

    db abstracted for example

--- src/base.js
+++ src/base.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseApp = initializeApp("ADD AUTHENTICATION OBJECT HERE");
+const firebaseApp = initializeApp("ADD AUTHENICATION OBJECT HERE");
 
 const auth = getAuth(firebaseApp);
 const store = getFirestore(firebaseApp);
